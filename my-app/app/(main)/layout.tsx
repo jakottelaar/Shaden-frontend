e1e7def090c554aa575e6377afe23e6f84fbcac6
jakottelaar <jarno.kottelaar@gmail.com>
2023-10-18 17:44:05 +0200

    Moved main page to /main folder

--- my-app/app/(main)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-const layout = ({ children }: { children: ReactNode }) => {
-  return <div className="h-screen bg-primary-100">{children}</div>;
-};
-
-export default layout;
