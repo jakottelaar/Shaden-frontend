47dd2fbccdd64a7711c39208efee82471e2a78d3
jakottelaar <jarno.kottelaar@gmail.com>
2023-11-28 15:40:33 +0100

    Seperated dm chatbutton from friendlistitem comp

---
+++ my-app/components/DmChatButton.tsx
@@ -0,0 +1,54 @@
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { getChannelByUserId, createDMChannel } from "@/service/channel-service";
+import useAxios from "@/lib/hooks/useAxios";
+
+const DmChatButton = ({ friendId }: { friendId: number }) => {
+  const router = useRouter();
+  const axios = useAxios();
+  const [channelId, setChannelId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchChannel = async () => {
+      try {
+        const channel = await getChannelByUserId(axios, friendId);
+
+        console.log(channel);
+
+        setChannelId(channel.channel_id);
+      } catch (error) {
+        const newChannel = await createDMChannel(axios, friendId);
+        setChannelId(newChannel.channel_id);
+      }
+    };
+    fetchChannel();
+  }, [friendId]);
+
+  const openDirectMessage = () => {
+    router.push(`/channels/${channelId}`);
+  };
+
+  return (
+    <button
+      className="group flex h-10 w-10 items-center justify-center rounded-full bg-neutral-700 transition-all duration-300 hover:bg-neutral-800"
+      onClick={openDirectMessage}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-6 w-6 stroke-white"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default DmChatButton;
--- my-app/components/FriendListItem.tsx
+++ my-app/components/FriendListItem.tsx
@@ -4,15 +4,10 @@ import { Avatar, AvatarFallback } from "./ui/avatar";
 import { Separator } from "./ui/separator";
 import { Friend } from "@/types/types";
 import UserProfileModal from "./UserProfileModal";
-import { useRouter } from "next/navigation";
+import DmChatButton from "./DmChatButton";
 
 const FriendListItem = ({ friend }: { friend: Friend }) => {
   console.log(friend);
-  const router = useRouter();
-
-  const openDirectMessage = () => {
-    router.push(`/channels/${friend.friendId}`);
-  };
 
   return (
     <div>
@@ -32,25 +27,7 @@ const FriendListItem = ({ friend }: { friend: Friend }) => {
           </div>
         </div>
         <div className="flex flex-row space-x-4">
-          <button
-            className="group flex h-10 w-10 items-center justify-center rounded-full bg-neutral-700 transition-all duration-300 hover:bg-neutral-800"
-            onClick={openDirectMessage}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-6 w-6 stroke-white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-              />
-            </svg>
-          </button>
+          <DmChatButton friendId={friend.friendId} />
           <UserProfileModal openModal={false} friend={friend} />
         </div>
       </div>
