b13e018541cb977633415179f7d1d3575c0d75d1
jakottelaar <jarno.kottelaar@gmail.com>
2023-12-12 15:09:06 +0100

    Created message request and response types

--- my-app/types/types.ts
+++ my-app/types/types.ts
@@ -14,6 +14,21 @@ export interface PendingFriend {
 
 export interface Channel {
   channel_id: number;
-  creator_id: number;
+  creator_id: number | undefined;
   participant_id: number;
+}
+
+export interface MessageRequest {
+  channel_id: number | undefined;
+  sender_id: number | undefined;
+  content: string;
+}
+
+export interface MessageResponse {
+  message_id: number;
+  channel_id: number;
+  sender_id: number;
+  content: string;
+  created_at: string;
+  last_modified_date: string;
 }
