b540d3624ee65e91c6263a2fa3ea25aeec7f5e11
jakottelaar <jarno.kottelaar@gmail.com>
2024-01-17 21:51:41 +0100

    Created a providers component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { AuthProvider } from "@/components/AuthProvider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>{children}</AuthProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
